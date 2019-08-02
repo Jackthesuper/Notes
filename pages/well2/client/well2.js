4693d598b9bdd19b99c4ff255760bf56e57d2031
Jackthesuper <jack.shenzijie@outlook.com>
2019-08-02 15:44:06 -0700

    implement counter upper lower limit

--- pages/well2/client/well2.js
+++ pages/well2/client/well2.js
@@ -6,6 +6,7 @@ Template.well2.onCreated(function() {
     counter3:1,
     counter4:0,
     counter5:1,
+    total:5,
   });
   console.log("creating the template");
   console.dir(this.state);
@@ -19,12 +20,12 @@ Template.well2.helpers({
 
   theTotal: function(){
     const instance = Template.instance();
-    return instance.state.get("counter1")+ instance.state.get("counter2")+ instance.state.get("counter3")+ instance.state.get("counter4")+ instance.state.get("counter5");
+    return instance.state.get("total");
   },
 
   theRest: function(){
     const instance = Template.instance();
-    return 10-(instance.state.get("counter1")+ instance.state.get("counter2")+ instance.state.get("counter3")+ instance.state.get("counter4")+ instance.state.get("counter5"));
+    return 10-(instance.state.get("total"));
 
   },
 });
@@ -50,13 +51,25 @@ Template.well2.events({
     "click .js-pusher": function(event,instance){
       const counter = "counter"+event.target.dataset.message;
       const c = instance.state.get(counter);
+      const t = instance.state.get("total");
+      if(t==10){
+        window.alert("You can't add more than 10 meals!");
+        return;
+      }
       instance.state.set(counter,c+1);
+      instance.state.set("total", t+1);
     },
 
     "click .js-puller": function(event,instance){
       const counter = "counter"+event.target.dataset.message;
       const c = instance.state.get(counter);
+      const t = instance.state.get("total");
+      console.log(c);
+      if(c==0){
+        return;
+      }
       instance.state.set(counter,c-1);
+      instance.state.set("total", t-1);
     },
 
 });
